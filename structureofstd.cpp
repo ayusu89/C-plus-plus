98588ac04d22ba4de7d583e42b39078fe1151012
ayusu89 <ayusapaudel_25@eemc.edu.np>
2026-05-26 16:36:37 +0545

    ADDONG C++ PROJRCTS

---
+++ structureofstd.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+struct student
+{
+    int id;
+    string name;
+    float marks;
+    void display()
+    {
+        cout<<"Name of student :"<<name<<endl;
+        cout<<"Marks of the student :"<<marks<<endl;
+        cout<<"ID of the student :"<<id<<endl;
+    }
+};
+int main()
+{
+    student s;
+    // Get input from the user
+    cout<<"Enter the name of the student :"<<endl;
+    cin>>s.name;
+
+    cout<<"Enter the marks of the student :"<<endl;
+    cin>>s.marks;
+
+    cout<<"Enter the id of the student :"<<endl;
+    cin>>s.id;
+
+    s.display(); //calling function
+    return 0;
+}
