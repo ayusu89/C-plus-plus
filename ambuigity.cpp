a218c74998ea22c8e962a4a5bb6f61e7538e9b36
ayusu89 <ayusapaudel_25@eemc.edu.np>
2026-07-06 20:17:58 +0545

    AMBUIGITY

---
+++ ambuigity.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class A
+{
+public:
+    void display()
+    {
+        cout<<"Hello from A"<<endl;
+    }
+};
+
+class B : virtual public A
+{
+};
+
+class C : virtual public A
+{
+};
+
+class D : public B, public C
+{
+};
+
+int main()
+{
+    D obj;
+    obj.display();
+
+    return 0;
+}
