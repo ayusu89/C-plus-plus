45e3f009d8b86d24b5de2257c0036d3b4ca499c7
ayusu89 <ayusapaudel_25@eemc.edu.np>
2026-07-06 20:45:30 +0545

    RESOLVED AMBUIGITY

---
+++ resolved.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class A 
+{
+public:
+    void show()
+ { 
+    cout << "Class A function\n";
+ }
+};
+
+class B : virtual public A 
+{ 
+
+};
+class C : virtual public A 
+{
+
+};
+
+class D : public B, public C
+ {
+
+};
+
+int main() 
+{
+    D obj;
+    obj.show();   // No ambiguity due to virtual inheritance
+    return 0;
+}
