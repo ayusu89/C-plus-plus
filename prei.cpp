7ecc1e4c4af6cf22c1985ae0d135cb55c67193d7
ayusu89 <ayusapaudel_25@eemc.edu.np>
2026-08-13 11:02:37 +0545

    friend

--- prei.cpp
+++ prei.cpp
@@ -1,32 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 class Num
 {
-    private :
-    int n ;
-    public :
-    void input (int x)
+private:
+    int n;
+
+public:
+    void input(int x)
     {
         n = x;
     }
-    void operator ++ ()
+
+    void operator++()
     {
-        n = ++n;
+        ++n;
     }
+
     void display()
     {
-
-        cout << n<<endl;
+        cout << n << endl;
     }
 };
+
 int main()
 {
     Num obj;
+
     obj.input(10);
-    cout<<"Before Increment :"<<endl;
+
+    cout << "Before Increment :" << endl;
     obj.display();
+
     ++obj;
-    cout<<"After preincrement : "<<endl;
+
+    cout << "After preincrement :" << endl;
     obj.display();
+
     return 0;
 }
