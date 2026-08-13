ae0e3a9127ed7e56863354e7b164048ea1ef23bb
ayusu89 <ayusapaudel_25@eemc.edu.np>
2026-08-13 13:08:00 +0545

    binary

---
+++ binary.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std ;
+class complex
+{
+    private :
+    int real ;
+    int imag ;
+    public :
+    complex ()
+    {
+
+    }
+     complex (int r , int i )
+     {
+        real = r ;
+        imag = i ;
+     }
+     void display()
+     {
+        cout<<real<<"+i"<<imag<<endl;
+     }
+     complex operator +( complex &c2)
+     {
+        complex temp;
+        temp.real =  real + c2.real;
+        temp.imag = imag  + c2.imag;
+        return temp;
+        
+     }
+};
+int main ()
+{
+    complex c1 (2 , 3);
+    complex c2 (4 , 5);
+    
+    cout << "c1 = ";
+    c1.display();
+    cout<<"c2 = ";
+    c2.display();
+    complex c3 ;
+    c3 = c1 + c2 ; // calls operator overloading 
+    cout<<"c3 = ";
+    c3.display();
+    return 0;
+}
